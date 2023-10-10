2056f3dbfaa5d101820bdd114c48642b83576eaf
SakalNatalia <sakalnatalia101@gmail.com>
2023-10-10 22:29:44 +0200

    fixxed that it was empty

---
+++ Program/Program.cs
@@ -0,0 +1,10 @@
+namespace salata{
+    class Cezar{
+        static void Main(string[] args)
+        {
+            string message = "Hello World!!";
+            Console.WriteLine(message);
+            
+        }
+    }
+}
